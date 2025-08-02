50a52d7944aaaeb5a1b93a79577e3909504c0145
rajjys <rajjysrachid@gmail.com>
2025-08-02 19:04:31 +0200

    No real  changes. Triggering new deploy action

--- app/(public)/news/page.tsx
+++ app/(public)/news/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewsPage = () => {
   return (
-    <div className='Flex items-center justify-center text-4xl font-bold h-screen'>
+    <div className='Flex items-center justify-center text-2xl font-bold'>
       <span>Pas Dactualites</span>
     </div>
   )
