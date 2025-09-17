57b9271a2a8968d30f956deafbfb6ac89992d238
rajjys <rajjysrachid@gmail.com>
2025-09-17 15:39:55 +0200

    Added placeholder code

---
+++ app/(app)/league/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeaguePosts = () => {
+  return (
+    <div>
+      En developpement. Revenez plus tard
+    </div>
+  )
+}
+
+export default LeaguePosts
---
+++ app/(app)/team/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeamPosts = () => {
+  return (
+    <div>
+      En developpement Revenez plus tard
+    </div>
+  )
+}
+
+export default TeamPosts
