05e3908608b038a0b2d10391bd22286e60e7d703
rajjys <rajjysrachid@gmail.com>
2025-10-18 20:59:55 +0100

    fix: Enhance the tenant landing page design and consistency

--- components/public/standings-table.tsx
+++ components/public/standings-table.tsx
@@ -1,13 +1,12 @@
 // Define the Standing type based on the backend response
 
-import Image from "next/image";
-import { Skeleton } from "../ui";
 import Link from "next/link";
-import { Standings } from "@/schemas";
+import { Avatar, Skeleton, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui";
+import { StandingsBasic } from "@/schemas";
 
 // --- StandingsTable Component ---
 interface StandingsTableProps {
-    standings: Standings[];
+    standings: StandingsBasic[];
     rowsToShow?: number;
     isLoading: boolean;
 }
@@ -24,6 +23,8 @@ const StandingsTable: React.FC<StandingsTableProps> = ({ standings, rowsToShow =
                         </div>
                         <Skeleton className="h-4 w-12" />
                         <Skeleton className="h-4 w-12" />
+                        <Skeleton className="h-4 w-12" />
+                        <Skeleton className="h-4 w-12" />
                     </div>
                 ))}
             </div>
@@ -38,66 +39,48 @@ const StandingsTable: React.FC<StandingsTableProps> = ({ standings, rowsToShow =
         );
     }
 
-    const standingsToDisplay = standings.slice(0, rowsToShow);
+    const trimmedStandings = standings.slice(0, rowsToShow);
 
     return (
-        <div className="overflow-x-auto no-scrollbar px-2 md:px-4">
-            <table className="min-w-full divide-y divide-gray-200 font-normal">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th className="py-2 text-left text-xs text-gray-500 uppercase tracking-wider">
-                            #
-                        </th>
-                        <th className="px-4 py-2 text-left text-xs text-gray-500 uppercase tracking-wider">
-                            Équipe
-                        </th>
-                        <th className="px-4 py-2 text-left text-xs text-gray-500 uppercase tracking-wider">
-                            Matchs
-                        </th>
-                        <th className="px-4 py-2 text-left text-xs font-bold text-gray-900 uppercase tracking-wider">
-                            Points
-                        </th>
-                        {/* You can add more columns here if needed */}
-                        <th className="px-4 py-2 text-left text-xs text-gray-500 uppercase tracking-wider">
-                            Forme
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                    {standingsToDisplay.map((standing) => (
-                        <tr key={standing.team.id}>
-                            <td className="py-2 whitespace-nowrap text-sm text-gray-900">
-                                {standing.rank}
-                            </td>
-                            <td className="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
-                                <Link href={`/teams/${standing.league.slug}/${standing.team.slug}`} className="flex items-center justify-start space-x-2 group">
-                                    <Image
-                                      className='h-8 w-8 rounded-full object-cover border border-gray-400 mr-2'
-                                      src={standing.team.businessProfile.logoAsset?.url || "https://placehold.co/40x40/cccccc/333333?text=${team.name.charAt(0)}"}
-                                      height={60}
-                                      width={60}
-                                      placeholder="blur"
-                                      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
-                                      alt={`${standing.team.shortCode} Logo`}
-                                      // onError={(e) => { e.currentTarget.src = `https://placehold.co/40x40/cccccc/333333?text=${team.name.charAt(0)}`; }}
-                                    />
-                                    <span className="group-hover:text-orange-900 transition-colors duration-300 ease-in-out">{standing.team.name}</span>
-                                </Link>
-                            </td>
-                            <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
-                                <span className="pl-4">{standing.gamesPlayed}</span>
-                            </td>
-                            <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-900 font-bold">
-                                <span className="pl-4">{standing.points}</span>
-                            </td>
-                            <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
-                                {standing.form}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+    <div className="overflow-x-auto no-scrollbar">
+      <Table>
+        <TableHeader className="border-blue-200">
+          <TableRow>
+            <TableHead className="text-center"></TableHead>
+            <TableHead className="min-w-[100px]">Equipes</TableHead>
+            <TableHead className="font-semibold text-center">Pts</TableHead>
+            <TableHead className="text-center">J</TableHead>
+            <TableHead className="text-center">G</TableHead>
+            <TableHead className="text-center">P</TableHead>
+            <TableHead className="text-center">N</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {trimmedStandings.map((item) => {
+            return (
+                  <TableRow key={item.team.id} className="cursor-pointer hover:bg-gray-100">
+                    <TableCell className="font-medium text-center px-0">{item.rank}</TableCell>
+                    <TableCell className="px-0">
+                      <Link href={`/teams/${item.team.league.slug}/${item.team.slug}`} className="flex items-center gap-3 w-full">
+                        <Avatar src={item.team.businessProfile?.logoAsset?.url} name={item.team.name} size={25} />
+                        <span className="hidden md:inline font-bold">
+                            {item.team.name}
+                            <span className="text-slate-400 font-normal text-xs"> ({item.team.shortCode})</span>
+                        </span>
+                        <span className="font-bold md:hidden">{item.team.shortCode}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="font-semibold text-center">{item.points}</TableCell>
+                    <TableCell className="text-center">{item.gamesPlayed}</TableCell>
+                    <TableCell className="text-center">{item.wins}</TableCell>
+                    <TableCell className="text-center">{item.losses}</TableCell>
+                    <TableCell className="text-center">{item.draws}</TableCell>
+                  </TableRow>
+            )})
+          }
+        </TableBody>
+      </Table>
+    </div>
     );
 };
 
